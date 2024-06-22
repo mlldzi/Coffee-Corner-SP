9f9c6dabe4fdf3a410c531eac85b6df1cb9b95b9
Denis <terehenkokiki@gmail.com>
2024-06-22 19:51:37 +1000

    Update page.js

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -5,7 +5,7 @@ import VideoContainer from '../components/VideoContainer';
 import styles from '../login/login.module.css';
 import Link from 'next/link';
 import useAuthStore from '../services/store';
-import { getUserProfile } from '../services/api'; // Импортируйте функцию getUserProfile
+import { getUserProfile } from '../services/api';
 
 const ProfilePage = () => {
   const { user, logout } = useAuthStore();
@@ -13,10 +13,14 @@ const ProfilePage = () => {
 
   useEffect(() => {
     const loadProfile = async () => {
-      if (user && user.userId) {
+      if (user && user.accessToken) {
         try {
-          const profileData = await getUserProfile(user.userId);
-          setProfile(profileData);
+          const profileData = await getUserProfile(user.accessToken);
+          if (profileData.success) {
+            setProfile(profileData.profile);
+          } else {
+            console.error('Failed to load profile data:', profileData.msg);
+          }
         } catch (error) {
           console.error('Error loading profile:', error);
         }
@@ -28,7 +32,6 @@ const ProfilePage = () => {
 
   const handleLogout = () => {
     logout();
-    // Перенаправление на страницу входа после выхода
     window.location.href = '/login';
   };
 
@@ -40,12 +43,9 @@ const ProfilePage = () => {
         <div className={styles.profileContainer}>
           {profile ? (
             <>
-              <h2>Welcome, {profile.username}!</h2>
-              <p className={styles.profileField}>Email: {profile.email}</p>
-              <p className={styles.profileField}>Full Name: {profile.fullName}</p>
-              <p className={styles.profileField}>Bonus Points: {profile.bonusPoints}</p>
-              <p className={styles.profileField}>Phone Number: {profile.phoneNumber}</p>
-              <p className={styles.profileField}>Address: {profile.address}</p>
+              <h2>Привет, {profile.full_name}!</h2>
+              <p className={styles.profileField}>Номер телефона: {profile.phone_number}</p>
+			  <p className={styles.profileField}>Бонусные баллы: {profile.bonus_points}</p>
             </>
           ) : (
             <h2>Welcome, Guest!</h2>
